f41f09fdffc2a660d4845c00e67539a5707b2997
Eduardo da Silva <eduardo.silva@gmail.com>
2021-01-28 13:07:19 -0300

    Django Project: category endpoints

---
+++ Backends/backendDjango/backend/core/views/category.py
@@ -0,0 +1,12 @@
+from rest_framework import viewsets
+
+from backend.core.serializers import CategorySerializer
+from backend.core.models import Category
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+# Create your views here.
