6ea41ea0b52b76e3fb071f6fa9f6abbe10ea7af1
Eduardo da Silva <eduardo.silva@gmail.com>
2021-01-28 13:47:19 -0300

    Django Project : added Swagger docs

--- Backends/backendDjango/backend/urls.py
+++ Backends/backendDjango/backend/urls.py
@@ -3,7 +3,23 @@ from django.urls import include, path
 
 from backend.core.router import router
 
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
+
+# schema_view =
+
 urlpatterns = [
     path("api/v1/", include(router.urls)),
     path("admin/", admin.site.urls),
+    path("docs/openapi-file", SpectacularAPIView.as_view(), name="schema"),
+    # Optional UI:
+    path(
+        "docs/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path("docs/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
 ]
